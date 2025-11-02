97fafff0972855aaa6b056e84993849a413184b8
ALNoman78 <abdullaalno078@gmail.com>
2025-11-02 20:55:06 +0600

    test file added

---
+++ First.js
@@ -0,0 +1,9 @@
+const course = { name : "Programming Hero "}
+const course1 = { name : " Next Level Web Development"}
+
+const courses =[
+    [course , "Level - 1"],
+    [course1 , "Level - 2"]
+]
+
+console.log(courses)
---
+++ test.js
@@ -0,0 +1,9 @@
+const obj = {
+    nextLevel : { courseId: "Level - 2"},
+    "Programming Hero" : { courseId : "Level -1"},
+    true : "this boolean key"
+}
+
+// console.log(obj.nextLevel);
+console.log(obj["Programming Hero"]); // we can access there what ever we want
+console.log(obj["true"]);
