c639407e59a494604aa2e095570472224f0218fc
ALNoman78 <abdullaalno078@gmail.com>
2025-11-06 22:23:06 +0600

    some practice

--- Critical Thinking P3/some.js
+++
@@ -1,5 +0,0 @@
-const numbers = [ 2, 4, 5, 6, 7, 8, 2 ]
-
-const someNumbers = numbers.some((number) => number % 2 === 0)
-
-console.log(some)
---
+++ anothersome.js
@@ -0,0 +1,13 @@
+const numbers = [ 2, 4, 5, 6, 7, 8, 2 ]
+
+const someNumbers = numbers.some((number) => number % 2 === 0) // we can find which data we have
+
+// console.log(someNumbers)
+
+
+const currentRole = [ "guest " , "editor", "admin"]
+const mainRole = ["admin" , "manager "]
+
+const role = mainRole.some((exceptRole) => currentRole.includes(exceptRole))
+
+console.log(role)
